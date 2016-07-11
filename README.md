db4cef7706c78c116e6915ee5530c97c90d9ed41
Digital Spider <dev@digitalspider.com.au>
2016-07-11 19:19:02 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Java LXD Client
+
+The Java LXD Client allows Java application to interact with Linux based lxd commands
+
+See:
+* [https://www.stgraber.org/2016/04/18/lxd-api-direct-interaction/]
+
+
---
+++ src/main/java/au/com/javacloud/lxd/App.java
@@ -0,0 +1,23 @@
+package au.com.javacloud;
+
+import org.apache.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class App {
+
+	private static final Logger LOG = Logger.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOG.info("LXC START");
+		try {
+		}
+		catch (Exception e) {
+			LOG.error(e,e);
+		}
+		LOG.info("LXC DONE");
+	}
+
+}
+		
