e4e2b73d806d8c5793df8f0b2cb2ed90bd54d96f
Digital Spider <dev@digitalspider.com.au>
2016-07-15 07:45:46 +1000

    $@

--- bin/jlxd.sh
+++ bin/jlxd.sh
@@ -1,1 +1,1 @@
-java -cp target/jlxd-jar-with-dependencies.jar au.com.javacloud.lxd.LxdServiceImpl $1 $2 $3 $4 $5 $6 $7 $8 $9
+java -cp target/jlxd-jar-with-dependencies.jar au.com.javacloud.lxd.LxdServiceImpl $@
