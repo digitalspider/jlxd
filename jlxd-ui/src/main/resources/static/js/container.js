139cf3028506b590949d5344819aaa1103fc063d
digitalspider <dev@digitalspider.com.au>
2017-08-29 20:13:08 +1000

    More code

--- jlxd-ui/src/main/resources/static/js/container.js
+++ jlxd-ui/src/main/resources/static/js/container.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+	// Search form
     $("#search-form").submit(function (event) {
         event.preventDefault();
         var placeholderEle = $("#containers");
@@ -7,6 +8,16 @@ $(document).ready(function () {
         var templatePath = "template/handlebars/container.html"
         fire_ajax_submit(postUrl, templatePath, placeholderEle);
     });
+    
+    // Add Server Form
+	$("#addServerForm").submit(function (event) {
+		event.preventDefault();
+	    var name = $("#addServerForm #name").val();
+	    var hostAndPort = $("#addServerForm #hostAndPort").val();
+	    var description = $("#addServerForm #description").val();
+	    var postUrl = "/server/add/"+name+"/"+hostAndPort+"/"+description;
+	    alert(postUrl);
+	});
 
     // Do a default search for all
     $("#bth-search").click();
