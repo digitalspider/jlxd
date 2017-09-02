8598836896736cb99f6c80853b451f34a4ec4ed9
David Vittor <davidv@booktopia.com.au>
2017-09-02 16:15:52 +1000

    Updated js and handlebars now in webjars

--- jlxd-core/src/test/java/au/com/jcloud/lxd/service/impl/LxdApiServiceImplTest.java
+++ jlxd-core/src/test/java/au/com/jcloud/lxd/service/impl/LxdApiServiceImplTest.java
@@ -3,6 +3,7 @@ package au.com.jcloud.lxd.service.impl;
 import org.junit.Assert;
 import org.junit.Test;
 
+import au.com.jcloud.lxd.LxdConstants;
 import au.com.jcloud.lxd.bean.LxdServerCredential;
 
 /**
@@ -11,24 +12,23 @@ import au.com.jcloud.lxd.bean.LxdServerCredential;
 public class LxdApiServiceImplTest {
 
 	private LxdApiServiceImpl lxdApiService = new LxdApiServiceImpl();
-	
+
 	@Test
 	public void getBaseUrl() {
 		String result = lxdApiService.getBaseUrl(null);
 		Assert.assertEquals(lxdApiService.CURL_URL_BASE_LOCAL, result);
-		
+
 		result = lxdApiService.getBaseUrl(new LxdServerCredential());
 		Assert.assertEquals(lxdApiService.CURL_URL_BASE_LOCAL, result);
-		
-		LxdServerCredential credential = new LxdServerCredential();
-		credential.setRemoteHostAndPort("test");
-		result = lxdApiService.getBaseUrl(credential);
-		Assert.assertEquals("curl -s -k --cert ~/.config/lxc/client.crt --key ~/.config/lxc/client.key https://test:8443", result);
 
-		credential.setRemoteHostAndPort("10.1.1.5:8080");
+		LxdServerCredential credential = new LxdServerCredential("test");
+		result = lxdApiService.getBaseUrl(credential);
+		Assert.assertEquals("curl -s -k --cert " + LxdConstants.DEFAULT_FILEPATH_REMOTE_CERT + " --key " + LxdConstants.DEFAULT_FILEPATH_REMOTE_KEY + " https://test:8443", result);
+
+		credential = new LxdServerCredential("10.1.1.5:8080", "~/.config/lxc/client.crt", "~/.config/lxc/client.key");
 		result = lxdApiService.getBaseUrl(credential);
 		Assert.assertEquals("curl -s -k --cert ~/.config/lxc/client.crt --key ~/.config/lxc/client.key https://10.1.1.5:8080", result);
-		
+
 		credential.setRemoteHostAndPort("10.1.1.5");
 		credential.setRemoteCert("/test/client.crt");
 		credential.setRemoteKey("/test/client.key");
@@ -36,14 +36,13 @@ public class LxdApiServiceImplTest {
 		Assert.assertEquals("curl -s -k --cert /test/client.crt --key /test/client.key https://10.1.1.5:8443", result);
 
 	}
-	
+
 	@Test
 	public void getParameterisedUrl() {
 		String url = "a${ID}:${ID}";
 		String id = "b";
-		String result = lxdApiService.getParameterisedUrl(url,id);
+		String result = lxdApiService.getParameterisedUrl(url, id);
 		Assert.assertEquals("ab:b", result);
 	}
-	
-}
 
+}
--- jlxd-ui/src/main/resources/static/js/image.js
+++ jlxd-ui/src/main/resources/static/js/image.js
@@ -8,48 +8,7 @@ $(document).ready(function () {
         fire_ajax_submit(postUrl, templatePath, placeholderEle);
     });
 
-    // Do a default search for all
-    $("#bth-search").click();
+    // initialise the page
+	getAllServers();
+	reloadImages();
 });
-
-function fire_ajax_submit(postUrl, templatePath, placeholderEle) {
-
-    var search = {}
-    search["searchTerm"] = $("#searchTerm").val();
-
-    placeholderEle.html("");
-    $("#feedback").html("");
-    $("#btn-search").prop("disabled", true);
-
-    $.ajax({
-        type: "POST",
-        contentType: "application/json",
-        url: postUrl,
-        data: JSON.stringify(search),
-        dataType: 'json',
-        cache: false,
-        timeout: 60000,
-        success: function (data) {
-
-            var json = "<h4>Ajax Response</h4><pre>" + JSON.stringify(data, null, 4) + "</pre>";
-            //$('#feedback').html(json);
-            if (data && data.result) {
-	            injectTemplatedContent(templatePath,data.result,placeholderEle);
-            }
-
-            console.log("SUCCESS : ", data);
-            $("#btn-search").prop("disabled", false);
-
-        },
-        error: function (e) {
-
-            var json = "<h4>Ajax Response</h4><pre>" + e.responseText + "</pre>";
-            $('#feedback').html(json);
-
-            console.log("ERROR : ", e);
-            $("#btn-search").prop("disabled", false);
-
-        }
-    });
-
-}
